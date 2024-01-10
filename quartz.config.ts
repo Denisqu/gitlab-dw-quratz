63be2f695e957e5217534333f14324813880348b
Пользователь <denis2000.kuzik@gmail.com>
2024-01-10 20:11:29 +0300

    Quartz sync: Jan 10, 2024, 8:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴Документация Gitlab-Downloader",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
